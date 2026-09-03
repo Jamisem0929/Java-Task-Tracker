b65c7f2d41cb35940105fac42d77bd1ed196f41a
jamisem0929 <ethan.jamison@washburn.edu>
2026-09-03 01:09:53 -0500

    Task Manager class And basic Task Handling

--- Java-Task-Tracker/TaskManager.java
+++ Java-Task-Tracker/TaskManager.java
@@ -1,38 +1,40 @@
 import java.util.ArrayList;
+
 public class TaskManager {
+
     private ArrayList<Task> tasks;
 
-    public TaskManager(ArrayList<Task> tasks){
-        this.tasks = tasks
+    public TaskManager(ArrayList<Task> tasks) {
+        this.tasks = tasks;
     }
 
-
-     public Task findTaskById(int id){
-        for(Task task : tasks){
-            if(task.getId() == id){
+    public Task findTaskById(int id) {
+        for (Task task : tasks) {
+            if (task.getId() == id) {
                 return task;
             }
         }
+
         return null;
     }
-    public void addTask(Task task){
-        task.add(task);
 
+    public void addTask(Task task) {
+        tasks.add(task);
     }
-    public void deleteTask(int id){
+
+    public void deleteTask(int id) {
         Task task = findTaskById(id);
 
-        if (task != null){
+        if (task != null) {
             tasks.remove(task);
         }
     }
-    public void completeTask(int id){
+
+    public void completeTask(int id) {
         Task task = findTaskById(id);
 
-        if (task != null){
+        if (task != null) {
             task.markComplete();
         }
     }
-
-
 }
