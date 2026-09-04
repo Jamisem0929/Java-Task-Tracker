ede3dceca1044db55c25a01cb0f579826d037c75
jamisem0929 <ethan.jamison@washburn.edu>
2026-09-04 00:07:21 -0500

    Added some buttons and some javafx effects

---
+++ Java-Task-Tracker/src/main/java/TaskTrackerApp.java
@@ -0,0 +1,64 @@
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.ComboBox;
+
+public class TaskTrackerApp extends Application {
+    
+
+    @Override
+    public void start(Stage stage){
+        VBox layout =new VBox();
+        Label titleLabel = new Label("Task Tracker");
+        layout.getChildren().add(titleLabel);
+
+        Scene scene = new Scene(layout, 500, 400);
+        stage.setScene(scene);
+        Button addButton = new Button("Add Task");
+
+        TextField titleField = new TextField();
+        titleField.setPromptText("Enter Task Title here");
+
+        TextArea descriptionArea = new TextArea();
+        descriptionArea.setPromptText("Enter Description here");
+
+        ComboBox<Priority> priorityBox = new ComboBox<>();
+        priorityBox.getItems().addAll(
+            Priority.LOW,
+            Priority.MEDIUM,
+            Priority.HIGH
+        )
+        priorityBox.setValue(Priority.MEDIUM);
+        addButton.setOnAction(event -> {
+            String title = titleField.getText();
+            System.out.println(title);
+
+            String description = descriptionArea.getText();
+            System.out.println(description);
+        });
+
+        
+        layout.getChildren().add(addButton);
+        layout.getChildren().add(titleField);
+        layout.getChildren().add(descriptionArea);
+
+
+
+
+        stage.setTitle("Task Tracker");
+        stage.show();
+    }
+    public static void main(String[] args){
+        launch(args);
+    }
+
+}
+
+
